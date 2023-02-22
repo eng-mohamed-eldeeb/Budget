629092c28fbdd56f178d9ad58ad8cb8fad3424db
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-02-23 00:12:27 +0200

    create most of the stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'splashs/index'
-  devise_for :users, path_names: { sign_in: "login", sign_out: "logout" }
-  resources :users, only: [:index]
-  root to: "users#index"
+  
+  devise_for :users
+  resources :users
+  resources :categories
+  resources :payments
+  resources :splash, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "categories#index"
 end
